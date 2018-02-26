ec9c60998738ca66a9009ee899893cc61925582c
Melanie Seltzer <melleh11@gmail.com>
2018-02-25 23:58:38 -0800

    Use dev/prod webpack configs, extract css on build and minimize, uglify the js, use gzip and brotli compression

--- src/server/express.js
+++ src/server/express.js
@@ -2,35 +2,44 @@ import express from 'express'
 const server = express()
 import path from 'path'
 
-const webpack = require('webpack')
-const config = require('../../config/webpack.dev.js')
-// Create and return a compiler
-const compiler = webpack(config)
+const isProd = process.env.NODE_ENV === "production"
+if (!isProd) {
+  const webpack = require('webpack')
+  const config = require('../../config/webpack.dev.js')
+  // Create and return a compiler
+  const compiler = webpack(config)
 
-// Use the compiler with the middleware
-// Pull the package and envoke the function
-// Takes two args - compiler and devServer portion of config
-const webpackDevMiddleware = require('webpack-dev-middleware')(
-  compiler,
-  config.devServer
+  // Use the compiler with the middleware
+  // Pull the package and envoke the function
+  // Takes two args - compiler and devServer portion of config
+  const webpackDevMiddleware = require('webpack-dev-middleware')(
+    compiler,
+    config.devServer
+  )
+
+  // Add hot reloading to the server
+  const webpackHotMiddleware = require('webpack-hot-middleware')(
+    compiler,
+    config.devServer
+  )
+
+  // Tell Express to use the middleware
+  server.use(webpackDevMiddleware)
+  server.use(webpackHotMiddleware)
+}
+
+// Serve from the public folder
+// with gzip compression
+const expressStaticGzip = require('express-static-gzip')
+server.use(
+  expressStaticGzip('public', {
+    enableBrotli: true
+  })
 )
 
-// Add hot reloading to the server
-const webpackHotMiddleware = require('webpack-hot-middleware')(
-  compiler,
-  config.devServer
-)
-
-// Tell Express to use the middleware
-server.use(webpackDevMiddleware)
-server.use(webpackHotMiddleware)
-
-// Serve from the dist folder
-const staticMiddleware = express.static('dist')
-server.use(staticMiddleware)
-
-// Listen on port 8080
-// Run this function after server has started
-server.listen(8080, () => {
-  console.log('Server is listening!')
+// Port set by Heroku dynamically
+// Or use 8080 in dev
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`)
 })
